537269eb91dfb540bfb4781a901f9aaa2702d7ed
Mustakim <m.m.kiron@gmail.com>
2023-07-21 00:48:20 +0600

    Add Luxon as a module and display date time on the page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import BookCollection from './modules/bookCollection.js';
 import navigation from './modules/navigation.js';
+import { DateTime } from './node_modules/luxon/src/luxon.js';
 
 const bookCollection = new BookCollection();
 const nav = new navigation();
@@ -8,3 +9,10 @@ const addBookForm = document.querySelector('.add-form');
 addBookForm.addEventListener('submit', (event) => bookCollection.addBook(event));
   
 bookCollection.displayBooks();
+
+const dateTime = document.querySelector('.date-time');
+
+setInterval(() => {
+    const date = DateTime.now();
+    dateTime.innerHTML = date.toLocaleString(DateTime.DATETIME_FULL);
+  }, 1000);
