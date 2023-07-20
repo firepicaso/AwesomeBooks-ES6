82608bf69e1cccac29b01a2f51488825f89cd6cf
Mustakim <m.m.kiron@gmail.com>
2023-07-21 00:58:50 +0600

    Fix linter errors

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 import BookCollection from './modules/bookCollection.js';
-import navigation from './modules/navigation.js';
+import Navigation from './modules/navigation.js';
 import { DateTime } from './node_modules/luxon/src/luxon.js';
 
 const bookCollection = new BookCollection();
-const nav = new navigation();
-  
+
 const addBookForm = document.querySelector('.add-form');
 addBookForm.addEventListener('submit', (event) => bookCollection.addBook(event));
-  
+
 bookCollection.displayBooks();
 
 const dateTime = document.querySelector('.date-time');
 
 setInterval(() => {
-    const date = DateTime.now();
-    dateTime.innerHTML = date.toLocaleString(DateTime.DATETIME_FULL);
-  }, 1000);
+  const date = DateTime.now();
+  dateTime.innerHTML = date.toLocaleString(DateTime.DATETIME_FULL);
+}, 1000);
+
+const nav = new Navigation();
+window.onload = (nav);
